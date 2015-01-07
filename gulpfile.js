b7602fd5b00de90e4ea787ad2bc9803f0e6065ee
Adam Thomas <adam.thomas@fivebyfiveuk.com>
2015-01-07 12:48:12 +0000

    work

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
+
+var getBundleName = function () {
+    var version = require('./package.json').version;
+    var name = require('./package.json').name;
+    return name + '-' + version;
+};
+
+gulp.task('default', ['js', 'css']);
+
+gulp.task('css', function() {
+    gulp.src('./lib/css/styles.css')
+        .pipe(gulp.dest('./dist/css/'));
+});
+
+
+gulp.task('js', function() {
+    gulp.src('./lib/js/index.js')
+        .pipe(rename(getBundleName() + '.js'))
+        .pipe(gulp.dest('./dist/js/'))
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(rename(getBundleName() + '.min.js'))
+        .pipe(gulp.dest('./dist/js/'));
+});
+
+
+gulp.task('watch', ['js', 'css'], function() {
+    gulp.watch('./lib/js/**/*.js', ['js']);
+    gulp.watch('./lib/css/**/*.js', ['css']);
+});
